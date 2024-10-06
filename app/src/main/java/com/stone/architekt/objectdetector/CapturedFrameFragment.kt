b656094671f948b3d7ff901597074b0d7b05c84a
buszi <klosz785@gmail.com>
2024-10-06 19:50:35 +0200

    add arrow back icon

--- app/src/main/java/com/stone/architekt/objectdetector/CapturedFrameFragment.kt
+++ app/src/main/java/com/stone/architekt/objectdetector/CapturedFrameFragment.kt
@@ -5,11 +5,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 import android.widget.ImageView
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
-import com.google.android.material.button.MaterialButton
+import com.stone.architekt.R
 import com.stone.architekt.databinding.FragmentCapturedframeBinding
 
 class CapturedFrameFragment : Fragment() {
@@ -17,7 +19,8 @@ class CapturedFrameFragment : Fragment() {
     private lateinit var viewModel: CapturedFrameViewModel
     private lateinit var binding: FragmentCapturedframeBinding
     private lateinit var imageView: ImageView
-    private lateinit var resetButton: MaterialButton
+
+    private lateinit var resetButton: ImageButton
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -34,10 +37,11 @@ class CapturedFrameFragment : Fragment() {
         setupObservers()
 //        val args by navArgs<CapturedFrameFragmentArgs>()
 //        loadImageFromUri(Uri.parse((args.imageUri)))
-        binding.btnReset.setOnClickListener {
+        resetButton.setOnClickListener {
+            resetButton.setColorFilter(ContextCompat.getColor(requireContext(),R.color.icon_active))
             findNavController().popBackStack()
+//            resetButton.setColorFilter(ContextCompat.getColor(requireContext(), R.color.white))
         }
-
         return binding.root
     }
 
